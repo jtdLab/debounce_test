84763b0510cda998f01f8d43b3ab0c3f5f29e1e7
Jonas Schlauch <mrjosch99@gmail.com>
2021-12-29 10:50:45 +0100

    reduce sample

--- lib/my_bloc.dart
+++ lib/my_bloc.dart
@@ -5,12 +5,10 @@ abstract class MyEvent {}
 
 class Increment extends MyEvent {}
 
-class Decrement extends MyEvent {}
-
 class MyBloc extends Bloc<MyEvent, int> {
   MyBloc() : super(0) {
     on<Increment>(
-      (event, emit) => emit(state + 1),
+      (_, emit) => emit(state + 1),
       transformer: (events, _) => events.debounceTime(
         const Duration(milliseconds: 300),
       ),
